bf90345f6c8b04eb4d4b0472a8d415567c4a0d4b
Brycen Dorsay <dorsay@live.ca>
2015-03-05 00:50:35 -0800

    Gruntfile skeleton

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task.
+  grunt.registerTask('default', ['concat', 'uglify']);
+
+};
